99242c1df9d71f4f1e3c64e895f56756ad4ffd2c
Pablo Terradillos <tehsis@gmail.com>
2012-08-02 17:02:30 -0300

    The card index action now respond with a json string

--- controllers/card_controller.js
+++ controllers/card_controller.js
@@ -3,12 +3,7 @@ var mongoose = require("mongoose");
 var index_action = function(req, res) {
   var models = require("../models");
   var cards = models.card.findOne({}, function(err, doc) {
-    res.render('card', 
-      {
-  	    title : "Tabu",
-        card: doc 
-      }
-    );
+  	res.send(JSON.stringify(doc));	
   });
  }
   
