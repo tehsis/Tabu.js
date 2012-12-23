306709aa2548d31bee755a98dab3b61082ae1532
Pablo Terradillos <tehsis@gmail.com>
2012-12-23 16:58:19 -0300

    Cleaning files

--- public/javascripts/tabu.js
+++ public/javascripts/tabu.js
@@ -1,9 +1,63 @@
-(function() {
-    require(["libs/underscore.js,", 
-      "libs/jquery-1.7.2.js",
-      "libs/backbone.js"],
-      function() {
-        console.log($);
-      }); 
-    
-}) ();
+$(function() {
+	Card = Backbone.Model.extend({
+		defaults: function() {
+			return {
+				used: false
+			}
+		},
+
+		use: function() {
+			this.used = true;
+		}
+
+	});
+
+	Deck = Backbone.Collection.extend({
+		model: Card,
+		url: "/card",
+	});
+
+	deck = new Deck;
+
+	var CardView = Backbone.View.extend({
+		tagName: "div",
+
+		model: Card,
+
+		template: _.template($("#card").html()),
+
+		initialize: function() {
+			this.model.bind("change", this.render, this);
+		},
+
+		render: function() {
+			this.$el.html(this.template(this.model.toJSON()));
+
+			return this;
+		}
+	});
+
+	var TabuView = Backbone.View.extend({
+		el: $("#tabu"),
+
+		initialize: function() {
+			deck.bind("reset", function() { 
+				var card = new CardView;
+				console.log(deck.at(20).toJSON());
+			}, this);
+
+			deck.fetch({
+				success: function() {
+					console.log("fetched sucessful");
+				}
+			});
+		},
+
+		render: function() {
+
+		}
+	});
+
+	var Tabu = new TabuView;
+
+});
