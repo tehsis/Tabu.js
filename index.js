4d505bf1aeca9fdd06b3d70c75cf93cee7be1724
Pablo Terradillos <tehsis@gmail.com>
2013-01-07 17:52:56 -0300

    changed order for routing

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = require('./common.js');
 var controllers = require('./controllers');
 
 // Routes
+app.use("/card", controllers.card.index);
 app.use("/", controllers.tabu.index); 
 
 // JSON Responses
-app.use("/card", controllers.card.index);
 
 app.listen(app.get('port'), function(){
   console.log("Tabu server listening on port " + app.get('port'));
