8ff17ea502ad5239d50b4b2df059b7c9378e3656
Pablo Terradillos <tehsis@gmail.com>
2013-01-11 13:37:10 -0300

    Added icons for webapp

--- public/javascripts/views/firefox-install.js
+++ public/javascripts/views/firefox-install.js
@@ -2,24 +2,24 @@ define(
 function() {
 	var manifest = window.location + "/manifest/tabu.webapp";
 	firefoxInstall = {
+		// At desktop and Android's devices result will be null until
+		// Firefox gets https://bugzilla.mozilla.org/show_bug.cgi?id=806597
+		// fixed. Meanwhile, I'll relay on localstorage to check
+		// whether app is installed or not. 
+		// This is not going to work if the application was uninstalled
+		// or if the user deletes its localstorage.
 		install: function() {
-			var request = navigator.mozApps.getSelf();
-			request.onsuccess = function() {
-				if (!request.result) {
-					// At desktop and Android's devices result will be null until
-					// Firefox gets https://bugzilla.mozilla.org/show_bug.cgi?id=806597
-					// fixed. Meanwhile, I'll relay on localstorage to check
-					// whether app is installed or not. 
-					// This is not going to work if the application was uninstalled
-					// or if the user deletes its localstorage.
-					if (!localStorage.getItem("installed")) { 
+			if (localStorage.getItem("installed") != "true") { 
+				var request = navigator.mozApps.getSelf();
+				request.onsuccess = function() {
+					if (!request.result) {
 						var install_request = navigator.mozApps.install(manifest); 	
 						install_request.onsuccess = function() {
 							localStorage.setItem('installed', true);
-						}
-					}
+						};
+					};
 
-				}
+				};
 			};
 		}
 	}
