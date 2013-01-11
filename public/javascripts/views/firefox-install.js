12369c38af2853df12a5721699ab6f68f24cde54
Pablo Terradillos <tehsis@gmail.com>
2013-01-10 21:00:23 -0300

    Added a temporary solution to Firefox's bug https://bugzilla.mozilla.org/show_bug.cgi?id=806597

--- public/javascripts/views/firefox-install.js
+++ public/javascripts/views/firefox-install.js
@@ -1,6 +1,4 @@
-define([
-], 
-
+define(
 function() {
 	var manifest = window.location + "/manifest/tabu.webapp";
 	firefoxInstall = {
@@ -8,10 +6,19 @@ function() {
 			var request = navigator.mozApps.getSelf();
 			request.onsuccess = function() {
 				if (!request.result) {
-					// This is not going to work on desktop until Firefox
-					// gets https://bugzilla.mozilla.org/show_bug.cgi?id=806597
-					// fixed.
-					navigator.mozApps.install(manifest); 	
+					// At desktop and Android's devices result will be null until
+					// Firefox gets https://bugzilla.mozilla.org/show_bug.cgi?id=806597
+					// fixed. Meanwhile, I'll relay on localstorage to check
+					// whether app is installed or not. 
+					// This is not going to work if the application was uninstalled
+					// or if the user deletes its localstorage.
+					if (!localStorage.getItem("installed")) { 
+						var install_request = navigator.mozApps.install(manifest); 	
+						install_request.onsuccess = function() {
+							localStorage.setItem('installed', true);
+						}
+					}
+
 				}
 			};
 		}
