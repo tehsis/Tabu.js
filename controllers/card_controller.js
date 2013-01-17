b886fb545221cef9b36d08c08fd84109e20bf9be
Pablo Terradillos <tehsis@gmail.com>
2013-01-17 07:03:15 -0300

    Added initial application cache

--- controllers/card_controller.js
+++ controllers/card_controller.js
@@ -4,12 +4,7 @@ var index_action = function(req, res) {
 	var models = require("../models");
   
   var cards = models.card.find({}, function(err, docs) {
-  	if (req.xhr) {
   	  res.send(JSON.stringify(docs));	
-  	  console.log("request");
-    } else {
-  	  res.send("Error");
-  	}
   });
 
  }
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var app = require('./common.js');
 var controllers = require('./controllers');
 
 // Routes
+app.get("/manifest/tabu.appcache", function(req, res, next) {
+	res.header("Content-Type", "text/cache-manifest");
+	next();
+})
+
 app.use("/card", controllers.card.index);
 app.use("/", controllers.tabu.index); 
 
