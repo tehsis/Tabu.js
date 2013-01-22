e616247d44a0a24e1618e12e9e2e84f1a21f1c17
Pablo Terradillos <tehsis@gmail.com>
2013-01-22 10:02:16 -0300

    Added build.js for require optimization and volofile to handle it

---
+++ build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: "./public",
+    paths: {
+    	zepto: "javascripts/libs/zepto/zepto",
+    	underscore: "javascripts/libs/underscore/underscore",
+		backbone: "javascripts/libs/backbone/backbone"
+    },
+    name: "javascripts/bootstrap",
+    out: "main-built.js"
+})
