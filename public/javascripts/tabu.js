042ce8a7b90df3c36a4f153ca5520eb3838753eb
Pablo Terradillos <pterradillos@panareadigital.com>
2014-12-16 12:57:13 -0300

    moved files for gh-pages

--- public/javascripts/tabu.js
+++ public/javascripts/tabu.js
@@ -1,9 +1,9 @@
 require.config({
-	baseUrl: "..",
+	baseUrl: "/public",
 	paths: {
-		zepto: "/javascripts/libs/zepto/zepto",
-		underscore: "/javascripts/libs/underscore/underscore",
-		backbone: "/javascripts/libs/backbone/backbone"
+		zepto: "javascripts/libs/zepto/zepto",
+		underscore: "javascripts/libs/underscore/underscore",
+		backbone: "javascripts/libs/backbone/backbone"
 	}
 });
 
